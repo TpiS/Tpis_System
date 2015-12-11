028dea6e7773d69ba448defd5cbac7543518f1b7
TpiS <tpis.ss.minded14@gmail.com>
2015-12-11 22:56:59 +0900

    jsonデータの受け取り

--- api_server.py
+++ api_server.py
@@ -1,30 +1,40 @@
 # -*- coding: utf-8 -*-
-from bottle import route, run, HTTPResponse, request, Bottle, response, static_file
+from bottle import route, run, HTTPResponse, request, Bottle, response, static_file, auth_basic, template
 import simplejson as json
+import json
 import datetime
 from StringIO import StringIO
-import classifier as c
+import classifier_csv as c
+import pymongo
+import urllib2
+
 
 app = Bottle()
-@app.route('/receiver', method="POST")
+@app.route('/receiver2', method="POST")
 def receiver():
     content_type = request.get_header('Content-Type')
-    if content_type  == 'audio/pcm' or content_type == "text/plain":
+    if content_type  == 'audio/wav' or content_type == "text/plain":
 #        print request.body.read()
         pass
     else:
         return "Bad Content-Type"
 
     id = request.get_header('X-Voice-ID')
-    speaker = "panda"
+    #speaker = "panda"
     print "ID=>"+str(id)
-    dir = "/home/sw/dataset"
+    dir = "/home/sugaya/public_html/wav/"
     tdatetime = datetime.datetime.now()
     tstr = tdatetime.strftime('%Y%m%d%H%M%S')
-    if content_type  == 'audio/pcm': #音源が与えられた場合
-        target_path = "/".join([dir, "wave", speaker+tstr+str(id)+".pcm"])
+
+
+
+    if content_type  == 'audio/wav': #音源が与えられた場合
+        target_path = "/".join([dir+"20151013223903.wav"])
+        print target_path
+        """
         with open(target_path, mode = 'w') as fh:
             fh.write(str(request.body.read()))
+        """
         #活性度推定
         result = c.classify_by_file(target_path)
         body = json.dumps(result)
@@ -33,13 +43,18 @@ def receiver():
         return r
     elif content_type  == 'text/plain': #音声認識結果が与えられた場合
         target_path = "/".join([dir, "text", speaker+tstr+str(id)+".rcg"])
+
         with open(target_path, mode = 'w') as fh:
             fh.write(str(request.body.read()))
     return "OK\r\n"
 
+
+
 @app.route('/saveWavPage', method="GET")
 def saveWavPage():
-    return static_file("index.html",root="/home/fujikura/public_html/Tpis_System")
+    return static_file("index.html",root="/home/sugaya/public_html/Tpis_System")
+
+
 
 @app.route('/saveWav', method=["OPTIONS","POST"])
 def saveWav():
@@ -56,4 +71,26 @@ def saveWav():
         return r
     print "saved "+target_path+" !!"
 
-run(app,host='0.0.0.0', port=9999, debug=True, reloader=True)
+
+
+@app.route('/subfomation', method=["OPTIONS","POST"])
+def subfomation():
+    print("subfomation")
+    
+    if request.is_ajax:
+        """
+        print(JSON.stringify(request.json))
+        """
+
+        json_data = json.loads(JSON.stringify(JSONdata))
+        print json_data
+
+    con = pymongo.MongoClient()
+    coll = con.test1.user
+
+    docs = [{"_id" : 5, "foo" : "Bye"}, {"_id" : 6, "Blah" : "Blue"}]
+
+    for doc in docs:
+        coll.save(doc)
+
+run(app,host='0.0.0.0', port=9990, debug=True, reloader=True)
